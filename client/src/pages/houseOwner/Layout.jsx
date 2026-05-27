1b5c8f9e585853e92c58cf03959d0f18aed02b42
Chaaben Amine <mohamedamine.benchaabene@esen.tn>
2026-05-27 19:07:30 +0100

    update

--- client/src/pages/houseOwner/Layout.jsx
+++ client/src/pages/houseOwner/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../components/houseOwner/temp'
+import Navbar from '../../components/houseOwner/Navbar'
 import Sidebar from '../../components/houseOwner/sidebar'
 import { Outlet } from 'react-router-dom'
 
